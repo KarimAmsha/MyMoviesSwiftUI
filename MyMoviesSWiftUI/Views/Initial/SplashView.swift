64e020c5cae72c081da0373129de503846676825
Karim Amsha <karim.amsha@gmail.com>
2023-06-17 23:54:24 +0300

    Splash View

--- MyMoviesSWiftUI/Views/Initial/SplashView.swift
+++ MyMoviesSWiftUI/Views/Initial/SplashView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("ic_logo")
+                .resizable()
+                .frame(width: UIScreen.main.bounds.width/1.5, height: UIScreen.main.bounds.width/6, alignment: .center)
+        }
     }
 }
 
